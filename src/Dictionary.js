5423ea63e02093236b45a62b6702a819f98bb3bd
Matt Delac <matt@delac.io>
2021-03-18 15:47:12 +0000

    Add api documentation URL

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,6 +12,7 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
+    // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
