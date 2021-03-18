925659c5c1f5326adf29012d6c5e13e7fe1b12ee
Matt Delac <matt@delac.io>
2021-03-18 15:27:23 +0000

    Add search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by SheCodes</small>
+        </footer>
+      </div>
     </div>
   );
 }
