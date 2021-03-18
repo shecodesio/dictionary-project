57dd8ce7c77d883dd22a90f6d5a39415ac417491
Matt Delac <matt@delac.io>
2021-03-18 15:30:03 +0000

    Add snapshot

---
+++ _snapshot/search/src/Dictionary.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
