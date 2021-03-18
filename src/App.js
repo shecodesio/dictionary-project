16997d5e2dec3b017a916b9b19c241d075446570
Matt Delac <matt@delac.io>
2021-03-18 15:00:26 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
