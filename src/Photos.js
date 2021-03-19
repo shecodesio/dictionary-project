2bcc209c8561441c58f204946f55201c21fdb4db
Matt Delac <matthieu.aussaguel@gmail.com>
2021-03-19 10:07:36 +0000

    Add alt attribute

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,11 @@ export default function Photos(props) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src={photo.src.landscape} className="img-fluid" />
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt={photo.src.photographer}
+                  />
                 </a>
               </div>
             );
