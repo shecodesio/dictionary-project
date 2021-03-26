f38d7034ff3790387b55573181fc8b2643bbd105
Matt Delac <matthieu.aussaguel@gmail.com>
2021-03-26 09:37:24 +0000

    Add alt

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" />
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
         <main>
           <Dictionary defaultKeyword="sunset" />
