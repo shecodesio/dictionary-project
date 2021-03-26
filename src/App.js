4ef404c4452428e61a206efe3b61d6b2a00140f7
Matt Delac <matthieu.aussaguel@gmail.com>
2021-03-26 09:36:30 +0000

    Remove alt

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <img src={logo} className="App-logo img-fluid" />
         </header>
         <main>
           <Dictionary defaultKeyword="sunset" />
